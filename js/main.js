3e258de78c303384b82a12753685249b4cfbebc9
matias delongaro <matiasadelongaro@gmail.com>
2024-03-15 16:45:36 -0300

    objetos

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function Jugador(nombre, posición, precio) {
+    this.nombre = nombre;
+    this.posición = posición;
+    this.precio = precio;
+}
+
+// ------------- Arqueros -------------
+
+const arquero1 = new Jugador ("Luis Ortiz", "Arquero", 1000 + " €")
+const arquero2 = new Jugador ("Pablo Arias", "Arquero", 700 +" €")
+const arquero3 = new Jugador ("Gonzalo Perez", "Arquero", 850 + " €")
+
+// ------------- Defensores -------------
+
+const defensor1 = new Jugador ("Gabriel Rueda", "Defensor", 1900 + " €")
+const defensor2 = new Jugador ("Ian Escobar", "Defensor", 1400 +" €")
+const defensor3 = new Jugador ("Pedro Lopez", "Defensor", 1300 + " €")
+const defensor4 = new Jugador ("Matías Piedra", "Defensor", 790 + " €")
+const defensor5 = new Jugador ("Miguel Lobato", "Defensor", 1250 +" €")
+const defensor6 = new Jugador ("Gerardo Ponte", "Defensor", 1100 + " €")
+
+// ------------- Mediocampistas -------------
+
+const mediocampista1 = new Jugador ("Hector Travolta", "Mediocampista", 2800 + " €")
+const mediocampista2 = new Jugador ("Julio Guzmán", "Mediocampista", 1700 +" €")
+const mediocampista3 = new Jugador ("Rodrigo Pereyra", "Mediocampista", 5400 + " €")
+const mediocampista4 = new Jugador ("Luis Gutierrez", "Mediocampista", 800 + " €")
+const mediocampista5 = new Jugador ("Hugo Gancedo", "Mediocampista", 1800 +" €")
+const mediocampista6 = new Jugador ("Dalmo Quispe", "Mediocampista", 680 + " €")
+const mediocampista7 = new Jugador ("Juan Palavecino", "Mediocampista", 2000 + " €")
+
+// ------------- Delanteros -------------
+
+const delantero1 = new Jugador ("Jacinto Parra", "Delantero", 3500 + " €")
+const delantero2 = new Jugador ("Sergio Palma", "Delantero", 1000 +" €")
+const delantero3 = new Jugador ("Lionel Masuero", "Delantero", 1900 + " €")
+const delantero4 = new Jugador ("Brian Geres", "Delantero", 2700 + " €")
