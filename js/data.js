4d50712d96a8163f7678e27863140d9fc0e68260
matias delongaro <matiasadelongaro@gmail.com>
2024-03-17 00:57:49 -0300

    busqueda

---
+++ js/data.js
@@ -0,0 +1,76 @@
+const jugadores = [
+    {
+        nombre: "Messi",
+        pais: "Argentina",
+        goles: 106,
+        partidos: 180,
+        promedio : 0.58,
+    },
+    {
+        nombre: "Neymar",
+        pais: "Brasil",
+        goles: 79,
+        partidos: 128,
+        promedio: 0.62,
+    },
+    {
+        nombre: "Pele",
+        pais: "Brasil",
+        goles: 77,
+        partidos: 92,
+        promedio: 0.84,
+    },
+    {
+        nombre: "Luis Suarez",
+        pais : "Uruguay",
+        goles: 68,
+        partidos: 138,
+        promedio: 0.49,
+    },
+    {
+        nombre: "Ronaldo",
+        pais: "Brasil",
+        goles: 62,
+        partidos: 98,
+        promedio: 0.63,
+    },
+    {
+        nombre: "Cavani",
+        pais: "Uruguay",
+        goles: 58,
+        partidos: 136,
+        promedio: 0.42,
+    },
+    {
+        nombre: "Batistuta",
+        pais: "Argentina",
+        goles: 56,
+        partidos: 76,
+        promedio: 0.72,
+    },
+    {
+        nombre: "Romario",
+        pais: "Brasil",
+        goles: 55,
+        partidos: 70,
+        promedio: 0.79,
+    },
+    {
+        nombre: "Alexis Sanchez",
+        pais: "Chile",
+        goles: 50,
+        partidos: 160,
+        promedio: 0.31,
+    },
+    {
+        nombre: "Zico",
+        pais: "Brasil",
+        goles: 48,
+        partidos: 71,
+        promedio: 0.68,
+    },
+
+
+
+]
+
